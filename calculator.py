8a0899837c8a070032da8c79067a8170585e0a68
raulalbertocalderonlopez <raul@badgermapping.com>
2022-05-13 12:48:12 +0200

    Refactor to reduce cyclomatic complexity

--- calculator.py
+++ calculator.py
@@ -1,15 +1,17 @@
 class Calculator:
 
+    def __init__(self):
+        pass
+
     SEPARATOR = ','
 
     def add(self, input_numbers):
         if self.is_two_numbers(input_numbers):
             split_numbers = input_numbers.split(self.SEPARATOR)
-            result = int(split_numbers[0]) + int(split_numbers[1])
-        else:
-            result = 0 if input_numbers == '' else int(input_numbers)
-
-        return result
+            return int(split_numbers[0]) + int(split_numbers[1])
+        elif input_numbers == '':
+            return 0
+        return int(input_numbers)
 
     def is_two_numbers(self, input_numbers):
         return self.SEPARATOR in input_numbers
